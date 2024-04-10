9dd2cdbb9e74e0d8945297179260313504229687
sardor <alimukhammad1@gmail.com>
2024-04-10 00:49:34 -0400

    basic loader

---
+++ script.js
@@ -0,0 +1,9 @@
+function login() {
+  console.log('login clicked');
+  document.getElementsByClassName("loader")[0].style.display = "block";
+}
+
+function register() {
+  console.log('Register clicked');
+  document.getElementsByClassName("loader")[0].style.display = "block";
+}
